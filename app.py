77260c50758e2f0ba5f03670624b01d8de5b48ae
Sam McCauley <sam@sams-air-2.home>
2019-10-29 23:40:19 -0400

    joins threads before exiting

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, Response
 from camera import CameraStream
 import numpy as np
 import cv2
-import time
+import time, signal
 app = Flask(__name__)
 
 # Get camera sources
@@ -15,19 +15,17 @@ global cap1, cap2
 cap1 = CameraStream(src=src1).start()
 if two_cameras: cap2 = CameraStream(src=src2).start()
 
-
+# Video streaming home page
 @app.route('/')
 def index():
-    """Video streaming home page."""
     return render_template('index.html')
 
 @app.route('/settings')
 def settings():
     return render_template('settings.html')
 
-
+# Generates the images for the video stream
 def gen_frame():
-    """Video streaming generator function."""
     if two_cameras: # Stitch two feeds together
         while cap1 and cap2:
             frame1 = cap1.frame
@@ -42,20 +40,17 @@ def gen_frame():
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + convert + b'\r\n') # Concate frame one by one and show result
 
+# Video streaming route. Put this in the src attribute of an img tag
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen_frame(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# Background process happening without any refreshing
-@app.route('/flip_cameras')
-def flip_cameras():
-    global cap1, cap2
-    if two_cameras:
-        temp = cap1
-        cap1 = cap2
-        cap2 = temp
+# Ends the video feed processes -- joins all threads
+def exit(sigal_num, signal_frame):
+    cap1.stop()
+    if two_cameras: cap2.stop()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, threaded=True)
+    signal.signal(signal.SIGINT, exit) # Calls exit function on ctrl^c
