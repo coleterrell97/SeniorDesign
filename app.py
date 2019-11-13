28d30bfd7c233adb1160fe01ef9ad8f51dc939d6
Shelby Stocker <shelbystocker@att.net>
2019-11-12 19:18:24 -0500

    Work on api for config. Needs to update values in config file. Needs to set slider values to current config values.

--- app.py
+++ app.py
@@ -1,9 +1,30 @@
-from flask import Flask, render_template, Response
+from flask import Flask, render_template, Response, request
 from camera import CameraStream
 import numpy as np
 import cv2
 import time, signal
+import json
+import re
 app = Flask(__name__)
+
+config_settings = {
+                    "CAP_PROP_BRIGHTNESS" : 0,
+                    "CAP_PROP_CONTRAST" : 15,
+                    "CAP_PROP_SATURATION" : 32,
+                    "CAP_PROP_SHARPNESS" : 16,
+                    "CAP_PROP_GAMMA" : 220,
+                    "CAP_PROP_WHITE_BALANCE_BLUE_U" : 5000,
+                    "CAP_PROP_GAIN" : 0,
+                    "CAP_PROP_PAN" : 0,
+                    "CAP_PROP_TILT" : 0,
+                    "CAP_PROP_ZOOM" : 100,
+                    "CAP_PROP_EXPOSURE" : 312,
+                    "CAP_PROP_BACKLIGHT" : 0,
+                    "CAP_PROP_ROLL" : 0,
+                    "CAP_PROP_IRIS" : 0,
+                    "CAP_PROP_FOCUS" : 0,
+                    "CAP_PROP_HUE" : 0
+                    }
 
 # Get camera sources
 two_cameras = input('Do you have two cameras (Y/N)? ').lower() == 'y'
@@ -19,6 +40,69 @@ if two_cameras: cap2 = CameraStream(src=src2).start()
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route("/update_values", methods=["POST"])
+def update_values():
+    print(config_settings)
+    #s = json.loads(config_settings)
+    brightness_update = request.form.get("brightness_slider")
+    config_settings["CAP_PROP_BRIGHTNESS"] = int(brightness_update)
+
+    contrast_update = request.form.get("contrast_slider")
+    config_settings["CAP_PROP_CONTRAST"] = int(contrast_update)
+
+    saturation_update = request.form.get("saturation_slider")
+    config_settings["CAP_PROP_SATURATION"] = int(saturation_update)
+
+    sharpness_update = request.form.get("sharpness_slider")
+    config_settings["CAP_PROP_SHARPNESS"] = int(sharpness_update)
+
+    gamma_update = request.form.get("gamma_slider")
+    config_settings["CAP_PROP_GAMMA"] = int(gamma_update)
+
+    white_update = request.form.get("white_slider")
+    config_settings["CAP_PROP_WHITE_BALANCE_BLUE_U"] = int(white_update)
+
+    gain_update = request.form.get("gain_slider")
+    config_settings["CAP_PROP_GAIN"] = int(gain_update)
+
+    Pan_update = request.form.get("Pan_slider")
+    config_settings["CAP_PROP_PAN"] = int(Pan_update)
+
+    tilt_update = request.form.get("tilt_slider")
+    config_settings["CAP_PROP_TILT"] = int(tilt_update)
+
+    zoom_update = request.form.get("zoom_slider")
+    config_settings["CAP_PROP_ZOOM"] = int(zoom_update)
+
+    exposure_update = request.form.get("exposure_slider")
+    config_settings["CAP_PROP_EXPOSURE"] = int(exposure_update)
+
+    backlight_update = request.form.get("backlight_slider")
+    config_settings["CAP_PROP_BACKLIGHT"] = int(backlight_update)
+
+    roll_update = request.form.get("roll_slider")
+    config_settings["CAP_PROP_ROLL"] = int(roll_update)
+
+    iris_update = request.form.get("iris_slider")
+    config_settings["CAP_PROP_IRIS"] = int(iris_update)
+
+    focus_update = request.form.get("focus_slider")
+    config_settings["CAP_PROP_FOCUS"] = int(focus_update)
+
+    hue_update = request.form.get("hue_slider")
+    config_settings["CAP_PROP_HUE"] = int(hue_update)
+
+    file_to_open = "./API_files/camera_props.json"
+    with open(file_to_open, "r+") as f:
+        data = f.read()
+        f.seek(0)
+        f.write(re.sub(r"<string>ABC</string>(\s+)<string>(.*)</string>", r"<xyz>ABC</xyz>\1<xyz>\2</xyz>", data))
+        f.truncate()
+
+    print(config_settings)
+    return render_template('index.html')
+
 
 @app.route('/settings')
 def settings():
